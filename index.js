6b8964d57dba9e1c9eb5667fec8af9594cd30b4e
David Gutierrez <andregutierrez159@gmail.com>
2019-06-04 02:33:37 -0500

    api rest termiando

--- index.js
+++ index.js
@@ -1,72 +1,89 @@
+const express = require('express')
 const fs = require('fs')
 const config = require('./utils/config')
-const express = require('express');
-const bodyParser = require('body-parser')
 
 // Variables
 const read = fs.createReadStream('./palabras/palabras.txt', 'utf8')
 const PORT = config.port;
 let longArray = []
 
-// Set up the express app
+// Levantamos el express app
 const app = express();
-// Parse incoming requests data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-// get all todos
+
+let longString = ''
+// Data por pedasos
+read.on('data', chunk => {
+    // construcción del string
+    longString += chunk
+})
+read.on('end', () => {
+    // Convertimso el string a Array
+    longArray = longString.split('\r\n')
+    // Levatamos el servidor
+    app.listen(PORT, () => {
+        console.log(`server running on port ${PORT}`)
+    });
+})
+// APi REST
 app.get(`/api`, (req, res) => {
+    // Validamos que exista el parametro query
+    let query = req.query.query ? req.query.query : ''
+    if (!query) {
+        res.status(400).send({
+            success: 'false',
+            message: 'parametro [query] es importante'
+        })
+        return
+    }
     let sizeArr = longArray.length
-    let wordKey = req.query.query ? req.query.query : '*ru??a'
     let controlTop = req.query.top ? parseInt(req.query.top) : Infinity
     let censitive = req.query.cs ? req.query.cs : false
     // Build
     let resultArray = []
     let rows = 0
-    let wordLike = wordKey.indexOf('*') >= 0 ? true : false
-    let wordLetters = wordKey.indexOf('?') >= 0 ? true : false
-    let cleanWord = wordKey.replace('*', '').replace(/\?/g, '.')
-    let regx = censitive ? new RegExp(`${cleanWord}`) : new RegExp(`${cleanWord}`, 'i')
+    
+    // BUIL A REGEX (https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/RegExp)
+    // Remplazamos todos los ? por caracteres posibles a través de "."
+    let builRegex = query.replace(/\?/g,'.')
+    
+    // Todos los que inicien y finalicen con "*"
+    if (/^\*.*\*$/.test(builRegex)) {
+        console.log('Ejemplo: *oa*, todos los que tienen "oa" en cualquier posicion')
+        builRegex = builRegex.replace(/\*/g, '.*')
+    // Todos los que contengan "*" pero no en los extremos
+    } else if (/^[^\*].*[^\*]$/.test(builRegex)) {
+        console.log('Ejemplo: o*a, todos los que inician con "o" y finalizan con "a"')
+        builRegex = `^${builRegex.replace(/\*/g, '.*')}$`
+    // Todos los que solo inicien con  "*"
+    } else if (/^\*.*/.test(builRegex)) {
+        console.log('Ejemplo: *a, todos los que finalizan con a')
+        builRegex = `${builRegex.replace(/\*/g, '.*')}$`
+    // Todos los que solo finalicen con  "*"
+    } else if (/.*\*$/.test(builRegex)) {
+        console.log('Ejemplo: a*, todos los que inicien con a')
+        builRegex = `^${builRegex.replace(/\*/g, '.*')}`
+    }
+    // texto censible
+    builRegex = censitive ? new RegExp(builRegex) : new RegExp(builRegex, 'i')
+    console.log(builRegex)
 
+    // recorremos cada fila para buscar el patron construido
     for (let i = 0; i < sizeArr; i++) {
         let word = longArray[i]
-        if (rows === controlTop) break
-        if (wordLike) {
-            if (regx.test(word)) {
-                resultArray.push(word)
-                rows++
-            }
-        } else {
-            if (wordLetters) {
-                let subWord = word.match(regx)
-                if(subWord){
-                    if (subWord[0] === word) {
-                        resultArray.push(word)
-                        rows++
-                    }
-                }
-            } else {
-                if (word === wordKey) {
-                    resultArray.push(word)
-                    rows++
-                }
-            }
+        // Si contiene top, entonces solo recorremos hasta esa cantidad
+        if (controlTop === rows) break
+        // Validamos el patron con el metodo EXEC, ya que solo nos trae BOOL y no todas las coincidencias, la cual
+        // nos da mayor perfomance
+        if (builRegex.exec(word)) {
+            // agregamos los resultado y contamos
+            resultArray.push(word)
+            rows++
         }
     }
-
-  res.status(200).send({
-    success: 'true',
-    total: resultArray.length,
-    param: resultArray
-  })
-})
-
-let longString = ''
-read.on('data', chunk => {
-    longString += chunk
-})
-read.on('end', () => {
-    longArray = longString.split('\r\n')
-    app.listen(PORT, () => {
-        console.log(`server running on port ${PORT}`)
-    });
+    // Retorna el resultado
+    res.status(200).send({
+        success: 'true',
+        total: resultArray.length,
+        result: resultArray
+    })
 })
